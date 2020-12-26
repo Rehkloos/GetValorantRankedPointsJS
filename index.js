2458d3905ca80943f3dfcf9f09aa11958067ee9b
Andre Saddler <dresadd09691@gmail.com>
2020-12-25 21:53:09 -0500

    Translate outputs to EN + Code reformatting

--- index.js
+++ index.js
@@ -7,99 +7,114 @@ moment.locale('pt-br');
 require('dotenv').config()
 
 axiosCookieJarSupport(axios);
- 
+
 const cookieJar = new tough.CookieJar();
+
+const username = process.env.USER;
+const pwd = process.env.PASSWORD;
 
 const auth = () => {
 
-  return new Promise((resolve, reject)=>{
+  return new Promise((resolve, reject) => {
 
     let data = {
       'client_id': 'play-valorant-web-prod',
-            'nonce': '1',
-            'redirect_uri': 'https://beta.playvalorant.com/opt_in',
-            'response_type': 'token id_token',
+      'nonce': '1',
+      'redirect_uri': 'https://beta.playvalorant.com/opt_in',
+      'response_type': 'token id_token',
     };
 
-    axios.post('https://auth.riotgames.com/api/v1/authorization', data, {jar: cookieJar, withCredentials: true})
-      .then(response=> {
-        
+    axios.post('https://auth.riotgames.com/api/v1/authorization', data, {
+        jar: cookieJar,
+        withCredentials: true
+      })
+      .then(response => {
+
         //create an .env file at the root of the project and add these variables
         data = {
-            'type': 'auth',
-            'username': process.env.USER,
-            'password': process.env.PASSWORD
+          'type': 'auth',
+          'username': username,
+          'password': pwd
         };
-        
-        axios.put('https://auth.riotgames.com/api/v1/authorization', data, {jar: cookieJar, withCredentials: true})
-        .then(response=>{
-          
-          let uri = response.data.response.parameters.uri;
-          let strTokens = uri.replace('https://beta.playvalorant.com/opt_in#', '').split('&');
 
-          let arrayTokens = {};
+        axios.put('https://auth.riotgames.com/api/v1/authorization', data, {
+            jar: cookieJar,
+            withCredentials: true
+          })
+          .then(response => {
 
-          strTokens.forEach(token=>{
-            arrayTokens[token.split('=')[0]] = token.split('=')[1];
-          });
+            let uri = response.data.response.parameters.uri;
+            let strTokens = uri.replace('https://beta.playvalorant.com/opt_in#', '').split('&');
 
-          //console.log('Access Token:', arrayTokens.access_token)
-          
+            let arrayTokens = {};
 
-          axios.defaults.headers.common['Authorization'] = `Bearer ${arrayTokens.access_token}`
-
-          axios.post('https://entitlements.auth.riotgames.com/api/token/v1', {}, {jar: cookieJar, withCredentials: true})
-          .then(response=>{
-
-            let entitlements_token = response.data.entitlements_token;
-            axios.defaults.headers.common['X-Riot-Entitlements-JWT'] = entitlements_token
-
-           //console.log('\nEntitlements Token:', entitlements_token);
-
-            axios.post('https://auth.riotgames.com/userinfo', {}, {jar: cookieJar, withCredentials: true})
-            .then(response=>{
-
-              let user_id = response.data.sub;
-              console.log('Player Id:', user_id);
-              resolve(user_id);
-
+            strTokens.forEach(token => {
+              arrayTokens[token.split('=')[0]] = token.split('=')[1];
             });
 
+            //console.log('Access Token:', arrayTokens.access_token)
+
+
+            axios.defaults.headers.common['Authorization'] = `Bearer ${arrayTokens.access_token}`
+
+            axios.post('https://entitlements.auth.riotgames.com/api/token/v1', {}, {
+                jar: cookieJar,
+                withCredentials: true
+              })
+              .then(response => {
+
+                let entitlements_token = response.data.entitlements_token;
+                axios.defaults.headers.common['X-Riot-Entitlements-JWT'] = entitlements_token
+
+                //console.log('\nEntitlements Token:', entitlements_token);
+
+                axios.post('https://auth.riotgames.com/userinfo', {}, {
+                    jar: cookieJar,
+                    withCredentials: true
+                  })
+                  .then(response => {
+
+                    let user_id = response.data.sub;
+                    console.log('Player Id:', user_id);
+                    resolve(user_id);
+
+                  });
+
+              });
+
           });
 
-        });
-
       })
-      .catch(error=> {
+      .catch(error => {
         reject(error);
       });
 
-    });
+  });
 }
 
 const getMovementString = movement => {
 
-  switch(movement){
+  switch (movement) {
 
     case 'INCREASE':
-      return 'Classificação aumentou';
+      return 'Rating increased';
     case 'MAJOR_INCREASE':
-      return 'Classificação aumentou muito';
+      return 'Rating increased a lot';
     case 'MINOR_INCREASE':
-      return 'Classificação aumentou um pouco';
+      return 'Rating increased slightly';
 
     case 'DECREASE':
-      return 'Classificação caiu';
+      return 'Rating fell';
     case 'MAJOR_DECREASE':
-      return 'Classificação caiu muito';
+      return 'Rating dropped a lot';
     case 'MINOR_DECREASE':
-      return 'Classificação caiu um pouco';
+      return 'Rating dropped a little';
 
     case 'PROMOTED':
-      return 'Promoção';
+      return 'Promotion';
 
     default:
-      return 'Rebaixamento';
+      return 'Demotion';
 
   }
 
@@ -109,30 +124,28 @@ const getRankString = rankId => {
 
   let rankName, rankNumber;
 
-  if(rankId < 6)
-    rankName = 'Ferro';
-  else if(rankId < 9)
+  if (rankId < 6)
+    rankName = 'Iron';
+  else if (rankId < 9)
     rankName = 'Bronze';
-  else if(rankId < 12)
+  else if (rankId < 12)
     rankName = 'Prata';
-  else if(rankId < 15)
-    rankName = 'Ouro';
-  else if(rankId < 18)
-    rankName = 'Platina';
-  else if(rankId < 21)
-    rankName = 'Diamante';
-  else if(rankId < 24)
-    rankName = 'Imortal';
+  else if (rankId < 15)
+    rankName = 'Gold';
+  else if (rankId < 18)
+    rankName = 'Platinum';
+  else if (rankId < 21)
+    rankName = 'Diamond';
+  else if (rankId < 24)
+    rankName = 'Immortal';
   else
-    return 'Radiante';
+    return 'Radiant';
 
-  if(((rankId / 3) % 1).toFixed(2) == 0.00){
+  if (((rankId / 3) % 1).toFixed(2) == 0.00) {
     rankNumber = 1;
-  }
-  else if(((rankId / 3) % 1).toFixed(2) == 0.33){
+  } else if (((rankId / 3) % 1).toFixed(2) == 0.33) {
     rankNumber = 2;
-  }
-  else{
+  } else {
     rankNumber = 3;
   }
 
@@ -146,16 +159,16 @@ const getRankedInfo = async (startIndex = 0, endIndex = '') => {
   let res = await axios.get(`https://pd.NA.a.pvp.net/mmr/v1/players/${playerId}/competitiveupdates?startIndex=${startIndex}&endIndex=${endIndex}`);
 
   let matches = res.data.Matches.reverse();
-  matches = matches.filter(match=>match.CompetitiveMovement != 'MOVEMENT_UNKNOWN'); //filtra as partidas rankeadas apenas;
+  matches = matches.filter(match => match.CompetitiveMovement != 'MOVEMENT_UNKNOWN'); //filtra as partidas rankeadas apenas;
 
-  if(matches.length){
+  if (matches.length) {
 
-    matches.forEach(match=>{
+    matches.forEach(match => {
 
-      console.log('\nPartida encontrada:', moment(match.MatchStartTime).format('DD/MM/YYYY HH:mm'));
-      console.log('Pontos antes:', match.TierProgressBeforeUpdate, '|| Pontos depois:', match.TierProgressAfterUpdate);
-      console.log('Rank antes:', getRankString(match.TierBeforeUpdate), '|| Rank depois:', getRankString(match.TierAfterUpdate));
-      console.log('Movimento:', getMovementString(match.CompetitiveMovement));
+      console.log('\nMatch found:', moment(match.MatchStartTime).format('DD/MM/YYYY HH:mm'));
+      console.log('Points before:', match.TierProgressBeforeUpdate, '|| Points after:', match.TierProgressAfterUpdate);
+      console.log('Rank before:', getRankString(match.TierBeforeUpdate), '|| Rank after:', getRankString(match.TierAfterUpdate));
+      console.log('Movement:', getMovementString(match.CompetitiveMovement));
 
     });
 
@@ -164,6 +177,3 @@ const getRankedInfo = async (startIndex = 0, endIndex = '') => {
 }
 
 getRankedInfo();
-
-
-
